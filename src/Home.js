8c722a0cd497636733aaf1268bd0c70e1175a12d
tejash09 <reddymallatejash@gmail.com>
2024-09-06 23:20:17 +0530

    pre-login page changes

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import "./Login.css";
 import { checkAuth } from "./ProtectedRoutes";
 import { useEffect, useState } from "react";
+import upstoxsrc from "./images/upstox_logo.svg";
+import kitesrc from "./images/kiteLogo.svg";
 
 function Home() {
   const login = async () => {
@@ -33,67 +35,66 @@ function Home() {
   }, []);
 
   if (checkAuth()) {
-    return (<div className="main-container">
-      <div className="sub-container-1">
-        <div className="sub-container-2">
-          <div className="sub-container-3">
-            <div>
-              <p><h2 >Trust with confidence</h2></p>
-              <br></br>
-              
-              <div className="description">
+    return (
+      <div className="main-container">
+        <div className="sub-container-1">
+          <div className="sub-container-2">
+            <div className="sub-container-3">
+              <div>
                 <p>
-                  <h3>No spam or gimmicks</h3>
-                  That's why 1.5+ crore customers trust Zerodha with ₹4.5+ lakh crores of equity investments and contribute to 15% of daily retail exchange volumes in India.
-                  <br></br>
-                  <br></br>
-                  <h3>Customer-first always</h3>
-                  That's why 1.5+ crore customers trust Zerodha with ₹4.5+ lakh crores of equity investments and contribute to 15% of daily retail exchange volumes in India.
-                  <br></br>
-                  <br></br>
-
-                  <h3>The Zerodha universe</h3>
-                  Not just an app, but a whole ecosystem. Our investments in 30+ fintech startups offer you tailored services specific to your needs.
+                  <h2>Trust with confidence</h2>
                 </p>
+                <br></br>
+
+                <div className="description">
+                  <p>
+                    <h3>No spam or gimmicks</h3>
+                    That's why 1.5+ crore customers trust Zerodha with ₹4.5+
+                    lakh crores of equity investments and contribute to 15% of
+                    daily retail exchange volumes in India.
+                    <br></br>
+                    <br></br>
+                    <h3>Customer-first always</h3>
+                    That's why 1.5+ crore customers trust Zerodha with ₹4.5+
+                    lakh crores of equity investments and contribute to 15% of
+                    daily retail exchange volumes in India.
+                    <br></br>
+                    <br></br>
+                    <h3>The Zerodha universe</h3>
+                    Not just an app, but a whole ecosystem. Our investments in
+                    30+ fintech startups offer you tailored services specific to
+                    your needs.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="sub-container-4">
-          
+          <div className="sub-container-4">
             <div className="pic">
-            <div class="pic1"></div>
-
+              <div class="pic1"></div>
             </div>
-
-          
+          </div>
         </div>
-        
+        <div className="sub-container-2">
+          <h2>Everything you need</h2>
+          <div className="sub-container-4">
+            <div className="pic4"></div>
+          </div>
+          <div className="sub-container-4">
+            <h2>Invest in everything</h2>
+            <br></br>
+            <p>
+              Online platform to invest in stocks, derivatives, mutual funds,
+              ETFs, bonds, and more.
+            </p>
+          </div>
+        </div>
       </div>
-      <div className="sub-container-2">
-        <h2>Everything you need</h2>
-        <div className="sub-container-4">
-          <div className="pic4"></div>
-        </div>
-        <div className="sub-container-4">
-          <h2>Invest in everything
-          </h2>
-          <br></br>
-          <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.
-          </p>
-        </div>
-
-
-
-      </div>
-      
-    </div>
-    
     );
   }
 
   return (
-<div className="main-container">
+    <div className="main-container">
       <div className="sub-container-1">
         <div className="sub-container-2">
           <div className="sub-container-3">
@@ -126,31 +127,46 @@ function Home() {
               <div className="button-grid">
                 <Button className="button" type="primary" onClick={login}>
                   <div className="button-content">
+                    <img src={upstoxsrc} alt="Upstox" />
                     <div>Upstox</div>
                   </div>
                 </Button>
                 <Button className="button" type="primary">
                   <div className="button-content">
+                    <img src={kitesrc} alt="Zerodha" />
                     <div>Zerodha</div>
                   </div>
                 </Button>
                 <Button className="button" type="primary">
                   <div className="button-content">
+                    <img
+                      src={require("./images/angelOne.png")}
+                      alt="Angel One"
+                    />
                     <div>Angel One</div>
                   </div>
                 </Button>
                 <Button className="button" type="primary">
                   <div className="button-content">
+                    <img
+                      src={require("./images/iciciLogo.png")}
+                      alt="Icici Direct"
+                    />
                     <div>Icici Direct</div>
                   </div>
                 </Button>
                 <Button className="button" type="primary">
                   <div className="button-content">
-                    <div>5Paise</div>
+                    <img
+                      src={require("./images/5paisaLogoNew.png")}
+                      alt="5Paisa"
+                    />
+                    <div>5Paisa</div>
                   </div>
                 </Button>
                 <Button className="button" type="primary">
                   <div className="button-content">
+                    <img src={require("./images/iiflLogo.png")} alt="IIFL" />
                     <div>IIFL</div>
                   </div>
                 </Button>
@@ -160,8 +176,7 @@ function Home() {
         </div>
       </div>
     </div>
-);
-
+  );
 }
 
 export default Home;
